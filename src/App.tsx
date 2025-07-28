af43ff3b47d9576c52947f7f1fa2536cce671c15
Nachito <ignacio.lm1991@gmail.com>
2025-07-28 17:38:23 -0300

    primer commit misa

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { useAudio } from './hooks/useAudio';
 import { MusicControl } from './components/MusicControl';
 import { Hero } from './components/Hero';
 import { ChurchSection } from './components/ChurchSection';
-import { PartySection } from './components/PartySection';
 import { PhotoCarousel } from './components/PhotoCarousel';
-import { MusicRequest } from './components/MusicRequest';
 import { ContactSection } from './components/ContactSection';
 import { Footer } from './components/Footer';
-
+import {  MusicWrapper } from './components/MusicWrapper';
 function App() {
   const { isPlaying, toggleMusic } = useAudio('https://www.soundjay.com/misc/sounds/bell-ringing-05.wav');
   
@@ -18,17 +16,18 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-50">
+      <MusicWrapper />
       <MusicControl isPlaying={isPlaying} onToggle={toggleMusic} />
       
       <Hero />
       
       <ChurchSection show={showChurchSection} />
       
-      <PartySection show={showPartySection} />
+    
       
       <PhotoCarousel />
       
-      <MusicRequest />
+
       
       <ContactSection />
       
